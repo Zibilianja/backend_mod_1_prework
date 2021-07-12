1717c17c8a02e9c812ca3fcb968648c8cd31af50
Jon Fuller <j.fuller.e@gmail.com>
2021-07-12 10:33:53 -0600

    complete ex1 ex2 and ex3

---
+++ section1/ex3.rb
@@ -0,0 +1,32 @@
+#prints I want to count my chickens
+puts "I will now count my chickens:"
+
+#prints the number of hens
+puts "Hens #{25 + 30 / 6}"
+
+#prints count eggs
+puts "Now I will count the eggs:"
+
+#prints the calculated number of eggs
+puts 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6
+
+#prints boolean question
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#prints boolean answer
+puts 3 + 2 < 5 - 7
+
+#prints question and answer for line 20 & 21
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - ? #{5 - 7}"
+
+#prints statement of dawning understanding
+puts "Oh, that's why it's false."
+
+#prints statement asking for more
+puts "How about some more."
+
+#prints boolean question and answer for 3 lines. 
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal #{5 <= -2}"
