5b354056a72b415c447af46a4e9dcbea8d82d7b8
Jon Fuller <j.fuller.e@gmail.com>
2021-07-12 15:43:54 -0600

    complete booleans, numbers, strings and variable exercises

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,59 +1,82 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
 # prints what that variable holds to the terminal:
-name = "Harry Potter"
+name = "Jonathan Fuller"
 p name
 
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+doby_message = "Harry Potter must not return to Hogwarts!"
+p doby_message
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
-p students
+p students + 2
+
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
-p students
+p students - 2
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
 
-# IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+first_name = "Jon"
+is_hungry = true
+number_of_pets = 3
 
-# Explain.
+p first_name
+p is_hungry
+p number_of_pets
+
+# IN WORDS:
+# How did you decide to use the data type you did for each of the three variables above?
+#Explain
+
+  #I chose to use the data types and values I did because the variable names were descriptive of what they required. First name is always going to be a string.
+  #is_hungry is clearly a boolean/true or false situation. Either a person is hungry or not. Finally, the last variable name has number within the name so using a number makes sense.
+  #You also can't really have half pets which is why I did not use a float value. I suppose you could argue if you and your partner shared a pet, that could be a half pet, but
+  #I don't know anyone who would count it as half a pet.
 
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
 
+first_name = "Jerry"
+is_hungry = false
+number_of_pets = 300
+
+p first_name
+p is_hungry
+p number_of_pets
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
+
 healthy_snacks = 6;
 junk_food_snacks = 8;
+total_snacks = healthy_snacks + junk_food_snacks
+p "The total number of snacks is #{total_snacks}"
 
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
+  #Yes, I did quite a number of times while completing it.
